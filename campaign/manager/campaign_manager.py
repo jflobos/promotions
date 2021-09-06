b7f8c177d57328b80e6710c97761932e21a9adf7
Juan Francisco Lobos Galilea <Juan Francisco Lobos Galilea>
2021-09-06 12:26:19 -0300

    add verify_email function to CampaignManager with tests

--- campaign/manager/campaign_manager.py
+++ campaign/manager/campaign_manager.py
@@ -1,3 +1,5 @@
+import json
+from django.core.signing import Signer
 from django.core.validators import EmailValidator
 from campaign.models import Campaign, Subscriber
 
@@ -9,15 +11,27 @@ class CampaignManager:
         subscriber = Subscriber.objects.create(
             campaign_id=campaign_id, 
             email=email, 
-            user_data=subscriber_data
+            user_data=subscriber_data,
+            verification_code=self.__create_verification_code(email=email, campaign_id=campaign_id)
         )
         return subscriber
+
+    def verify_email(self, verification_code) -> bool:
+        subscriber = Subscriber.objects.filter(verification_code=verification_code).first()
+        if subscriber is None:
+            raise ValueError("Invalid token")
+        if subscriber.verified:
+            raise ValueError("User already validated")
+        subscriber.verified = True
+        subscriber.save()
+        return True
+
 
     def __check_valid_inputs(self, campaign_id, email):
         self.__check_campaign(campaign_id=campaign_id)
         self.__check_valid_email(email=email)
         self.__check_repeteated_email(campaign_id=campaign_id,email=email)
-    
+
     def __check_campaign(self, campaign_id):
         if not Campaign.objects.filter(id=campaign_id).exists():
             raise ValueError("Invalid Campaign")
@@ -26,7 +40,12 @@ class CampaignManager:
         validator = EmailValidator()
         validator(email)
     
+    def __create_verification_code(self, email, campaign_id) -> str:
+        code = { "email": email, "campaign_id": campaign_id }
+        signer = Signer()
+        return signer.sign(json.dumps(code))
+        
+    
     def __check_repeteated_email(self, campaign_id, email) -> bool:
         if Subscriber.objects.filter(campaign_id=campaign_id, email=email).exists():
             raise ValueError("Email already registered") 
-
--- campaign/manager/tests/test_campaign_manager.py
+++ campaign/manager/tests/test_campaign_manager.py
@@ -1,3 +1,5 @@
+import json
+from django.core.signing import Signer
 from django.test import TestCase
 from django.core.validators import ValidationError
 from campaign.manager.campaign_manager import CampaignManager
@@ -9,6 +11,7 @@ class CampaignManagerTestCase(TestCase):
         self.campaign_manager = CampaignManager()
         self.subscriber_email = 'john@doe.com'
         self.subscriber_data = '{"name": "John", "last_name": "Doe"}'
+        self.subscriber_verification_code = self.__get_verification_code(self.subscriber_email, self.campaign.id)
         return super().setUp()
 
     def test_add_subscriber_base_case(self):
@@ -21,7 +24,8 @@ class CampaignManagerTestCase(TestCase):
         self.assertEqual(subscriber.campaign.id, self.campaign.id)
         self.assertEqual(subscriber.email, self.subscriber_email)
         self.assertEqual(subscriber.user_data, self.subscriber_data)
-        
+        self.assertFalse(subscriber.verified)
+        self.assertEqual(subscriber.verification_code, self.subscriber_verification_code)
 
     def test_add_subscriber_check_campaign_exists(self):
         with self.assertRaises(ValueError) as error:
@@ -45,4 +49,34 @@ class CampaignManagerTestCase(TestCase):
                 email=repeteated_email, 
                 subscriber_data= self.subscriber_data
             )
+
+    def test_verify_email_base_case(self):
+        email = "john+02@doe.com"
+        self.campaign_manager.add_subscriber(
+            campaign_id=self.campaign.id, 
+            email=email, 
+            subscriber_data=self.subscriber_data
+        )
+        verification_code = self.__get_verification_code(email=email, campaign_id=self.campaign.id)
+        value = self.campaign_manager.verify_email(verification_code=verification_code)
+        self.assertTrue(value)
+    
+    def test_verify_email_should_update_user_verify_field(self):
+        email = "john+03@doe.com"
+        subscriber = self.campaign_manager.add_subscriber(
+            campaign_id=self.campaign.id, 
+            email=email, 
+            subscriber_data=self.subscriber_data
+        )
+        self.assertFalse(subscriber.verified)
+        verification_code = self.__get_verification_code(email=email, campaign_id=self.campaign.id)
+        value = self.campaign_manager.verify_email(verification_code=verification_code)
+        self.assertTrue(value)
+        updated_subscriber = Subscriber.objects.get(pk=subscriber.pk)
+        self.assertTrue(updated_subscriber.verified)
+
+    def __get_verification_code(self, email, campaign_id):
+        self.signer = Signer()
+        code = {"email": email, "campaign_id": campaign_id}
+        return self.signer.sign(json.dumps(code))
 
