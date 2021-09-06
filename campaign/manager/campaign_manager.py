317abd2b08a00afef71f8672b39436b85191b505
Juan Francisco Lobos Galilea <Juan Francisco Lobos Galilea>
2021-09-06 12:35:06 -0300

    refactor CampaignManager to simplify code

--- campaign/manager/campaign_manager.py
+++ campaign/manager/campaign_manager.py
@@ -1,18 +1,16 @@
-import json
-from django.core.signing import Signer
-from django.core.validators import EmailValidator
+from campaign.manager.subscriber_helpers import create_verification_code, validate_subscriber
 from campaign.models import Campaign, Subscriber
 
 class CampaignManager:
     """Business logic implementation"""
 
     def add_subscriber(self, campaign_id, email, subscriber_data) -> Subscriber:
-        self.__check_valid_inputs(campaign_id=campaign_id, email=email)
+        validate_subscriber(campaign_id=campaign_id, email=email)
         subscriber = Subscriber.objects.create(
             campaign_id=campaign_id, 
             email=email, 
             user_data=subscriber_data,
-            verification_code=self.__create_verification_code(email=email, campaign_id=campaign_id)
+            verification_code=create_verification_code(email=email, campaign_id=campaign_id)
         )
         return subscriber
 
@@ -26,26 +24,3 @@ class CampaignManager:
         subscriber.save()
         return True
 
-
-    def __check_valid_inputs(self, campaign_id, email):
-        self.__check_campaign(campaign_id=campaign_id)
-        self.__check_valid_email(email=email)
-        self.__check_repeteated_email(campaign_id=campaign_id,email=email)
-
-    def __check_campaign(self, campaign_id):
-        if not Campaign.objects.filter(id=campaign_id).exists():
-            raise ValueError("Invalid Campaign")
-
-    def __check_valid_email(self, email):
-        validator = EmailValidator()
-        validator(email)
-    
-    def __create_verification_code(self, email, campaign_id) -> str:
-        code = { "email": email, "campaign_id": campaign_id }
-        signer = Signer()
-        return signer.sign(json.dumps(code))
-        
-    
-    def __check_repeteated_email(self, campaign_id, email) -> bool:
-        if Subscriber.objects.filter(campaign_id=campaign_id, email=email).exists():
-            raise ValueError("Email already registered") 
---
+++ campaign/manager/subscriber_helpers.py
@@ -0,0 +1,26 @@
+import json
+from django.core.signing import Signer
+from django.core.validators import EmailValidator
+from campaign.models import Campaign, Subscriber
+
+def validate_subscriber(campaign_id, email):
+    check_campaign(campaign_id=campaign_id)
+    check_valid_email(email=email)
+    check_repeteated_email(campaign_id=campaign_id,email=email)
+
+def check_campaign(campaign_id):
+    if not Campaign.objects.filter(id=campaign_id).exists():
+        raise ValueError("Invalid Campaign")
+
+def check_valid_email(email):
+    validator = EmailValidator()
+    validator(email)
+    
+def check_repeteated_email(campaign_id, email) -> bool:
+    if Subscriber.objects.filter(campaign_id=campaign_id, email=email).exists():
+        raise ValueError("Email already registered") 
+
+def create_verification_code(email, campaign_id) -> str:
+    code = { "email": email, "campaign_id": campaign_id }
+    signer = Signer()
+    return signer.sign(json.dumps(code))
