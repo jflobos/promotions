87fb519d7c8c6af0b202f7cebe1aa01adbec4cd6
Juan Francisco Lobos Galilea <Juan Francisco Lobos Galilea>
2021-09-06 09:43:53 -0300

    add campaign and subscriber models

--- campaign/models.py
+++ campaign/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Campaign(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=False)
+
+class Subscriber(models.Model):
+    campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE)
+    email = models.EmailField(unique=True)
+    user_data = models.TextField(null=False, blank=False)
