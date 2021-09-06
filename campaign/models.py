2a85b2cb3a3813958a0e03c6c831aabd9f2020fc
Juan Francisco Lobos Galilea <Juan Francisco Lobos Galilea>
2021-09-06 12:25:18 -0300

    add verified and verification_code fields to Subscriber class

--- campaign/models.py
+++ campaign/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.fields import BigAutoField
 
 class Campaign(models.Model):
     title = models.CharField(max_length=255)
@@ -8,3 +9,6 @@ class Subscriber(models.Model):
     campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE)
     email = models.EmailField(unique=True)
     user_data = models.TextField(null=False, blank=False)
+    verified = models.BooleanField(default=False, null=False, blank=False)
+    verification_code = models.TextField(null=False, default='')
+
